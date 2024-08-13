fab3381626936272083efcca3b50ca682fae715a
BBarricklow <BLBarricklow@gmail.com>
2024-08-13 00:20:36 -0700

    First commit

---
+++ database.js
@@ -0,0 +1,56 @@
+const database = {
+    "piccolo" : {
+        "name" : "Piccolo",
+        "price" : "$29.99",
+        "release-num" : 1,
+        "release-type" : "Retail",
+        "release-date" : "November 21 2009",
+        "tamashii-link" : "https://tamashiiweb.com/item/1172/?wovn=en",
+        "ee-link" : "https://www.entertainmentearth.com/product/dragon-ball-z-piccolo-sh-figuarts-action-figure/ba59914"
+    },
+    "super saiyan son goku" : {
+        "name" : "Super Saiyan Son Goku",
+        "price" : "$26.99",
+        "release-num" : 2,
+        "release-type" : "Retail",
+        "release-date" : "January 23 2010",
+        "tamashii-link" : "https://tamashiiweb.com/item/1216/?wovn=en",
+        "ee-link" : "https://www.entertainmentearth.com/product/dragon-ball-z-super-saiyan-goku-sh-figuarts-action-figure/ba59912"
+    },
+    "super saiyan son gohan" : {
+        "name" : "Super Saiyan Son Gohan",
+        "price" : "$29.99",
+        "release-num" : 3,
+        "release-type" : "Retail",
+        "release-date" : "July 17 2010",
+        "tamashii-link" : "https://tamashiiweb.com/item/1216/?wovn=en",
+        "ee-link" : "https://www.entertainmentearth.com/product/dragon-ball-z-son-gohan-sh-figuarts-action-figure/ba61922"
+    },
+    "super saiyan son goku sdcc 2011" : {
+        "name" : "Super Saiyan Son Goku (Special Color Edition)",
+        "price" : "$35",
+        "release-num" : 4,
+        "release-type" : "Event",
+        "release-date" : "July 21 2011",
+        "tamashii-link" : "Not Available",
+        "ee-link" : "Not Available"
+    },
+    "super saiyan vegeta" : {
+        "name" : "Super Saiyan Vegeta",
+        "price" : "$39.99",
+        "release-num" : 5,
+        "release-type" : "Premium Bandai",
+        "release-date" : "December 2011",
+        "tamashii-link" : "Not Available",
+        "ee-link" : "https://www.entertainmentearth.com/product/dragon-ball-z-super-saiyan-vegeta-sh-figuarts-figure/ba62466"
+    },
+    "trunks" : {
+        "name" : "Trunks",
+        "price" : "$48.99",
+        "release-num" : 6,
+        "release-type" : "Premium Bandai",
+        "release-date" : "May 2012",
+        "tamashii-link" : "https://tamashiiweb.com/item/11548/?wovn=en",
+        "ee-link" : "https://www.entertainmentearth.com/product/dragon-ball-z-trunks-sh-figuarts-action-figure/ba73944"
+    }
+}
