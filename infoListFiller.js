ea642dbaca382a17128a2e07e880ba47470dbcbe
BBarricklow <BLBarricklow@gmail.com>
2024-10-29 13:08:30 -0700

    Refine info style and seperate month and year data

--- infoListFiller.js
+++ infoListFiller.js
@@ -1,9 +1,10 @@
 let itemNum = document.getElementById("itemNum").innerText;
 let salesType = database[itemNum].releaseType;
-let releaseDate = database[itemNum].releaseDate;
+let releaseMonth = database[itemNum].releaseMonth;
+let releaseYear = database[itemNum].releaseYear;
 let price = database[itemNum].price;
 
-document.querySelector('#info-list').innerHTML += "<li>" + "Release Date: " + releaseDate + "</li>";
+document.querySelector('#info-list').innerHTML += "<li>" + "Release Date: " + releaseMonth + " " + releaseYear + "</li>";
 document.querySelector('#info-list').innerHTML += "<li>" + "Price: (USA) " + price + "</li>";
 document.querySelector('#info-list').innerHTML += "<li>" + "Release Type: " + salesType + "</li>";
 
