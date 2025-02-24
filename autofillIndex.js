d5d774ea6f089264f00ae8eb8ccfab40b5ca1840
BBarricklow <BLBarricklow@gmail.com>
2025-02-23 23:50:11 -0800

    Adjust line height on autofill list items

--- autofillIndex.js
+++ autofillIndex.js
@@ -1,4 +1,4 @@
 for (let i = 0; i < database.length; i++) {
-    document.querySelector('#autofill-list').innerHTML += "<li><a href='" + database[i].pagelink +"'>" + database[i].name + "</a></li>";
+    document.querySelector('#autofill-list').innerHTML += "<li  class='autolist-item'><a href='" + database[i].pagelink +"'>" + database[i].name + "</a></li>";
 
 }
