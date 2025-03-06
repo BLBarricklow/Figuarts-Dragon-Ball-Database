7af095d3292b417cf55fc145093c8767d6fd3c86
BBarricklow <BLBarricklow@gmail.com>
2025-03-05 22:02:49 -0800

    Add regions to data filler for pages

--- infoListFiller.js
+++ infoListFiller.js
@@ -3,10 +3,11 @@ let salesType = database[itemNum].releaseType;
 let releaseMonth = database[itemNum].releaseMonth;
 let releaseYear = database[itemNum].releaseYear;
 let price = database[itemNum].price;
+let regions = database[itemNum].region;
 
 document.querySelector('#info-list').innerHTML += "<li>" + "Release Date: " + releaseMonth + " " + releaseYear + "</li>";
 document.querySelector('#info-list').innerHTML += "<li>" + "Price: (USA) " + price + "</li>";
 document.querySelector('#info-list').innerHTML += "<li>" + "Release Type: " + salesType + "</li>";
+document.querySelector('#info-list').innerHTML += "<li>" + "Regions: " + regions.join(", ");
 
-
-console.log(salesType)
+//console.log(salesType)
